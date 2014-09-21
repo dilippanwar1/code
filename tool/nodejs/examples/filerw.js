d8e72331df9410a34b231fb67259bef74f0aa8bd
Deyuan Deng <deyuan.deng@gmail.com>
2014-09-21 11:43:27 -0400

    Express, passport.

--- tool/nodejs/examples/filerw.js
+++ tool/nodejs/examples/filerw.js
@@ -1,9 +1,27 @@
-var fs = require('fs')
+var fs = require('fs');
 
-fs.writeFile('Hangout', '...', function() {
-  console.log("can't wait to hear back from her!");
-});
+function example1() {
+  fs.writeFile('Hangout', '...', function() {
+    console.log("can't wait to hear back from her!");
+  });
 
-fs.readFile('Hangout', function(err, data) {
-  console.log("oh, look at all my money: " + data);
-});
+  fs.readFile('Hangout', function(err, data) {
+    console.log("oh, look at all my money: " + data);
+  });
+}
+
+function example2() {
+  // 'readdirSync' reads directory synchronously, and return an array
+  // of file/directory names.  'map' is a method on array, takes each
+  // element of the array, then return the modified element.
+  data = fs.readdirSync('.').map(
+    function(file) {
+      return file.slice(1);
+    }
+  );
+  console.log(data);
+}
+
+
+//example1();
+example2();
