8e7f619b3dc5d02168d89b43b3d1a4d29f16b2e2
Deyuan Deng <deyuan.deng@gmail.com>
2014-09-24 01:02:03 -0400

    Go variadic; pymox.

--- language/go/example/variadic.go
+++ language/go/example/variadic.go
@@ -13,6 +13,19 @@ func sum(num int, items ...int) int {
 	return total
 }
 
+func fn(args ...string) {
+	fmt.Println(args[0])
+	fmt.Println(args[1])
+	fmt.Printf("The whole slice: %v\n", args)
+}
+
 func main() {
 	fmt.Println(sum(5, 1, 2, 3, 4, 5))
+	fmt.Println("--")
+	fn("Hey", "there")
+	fmt.Println("--")
+	slice := []string{"Hello", "to", "you", "too"}
+	// When calling a function, if there are several arguments in a slice, it
+	// will unpack them and pass as separate arguments to a variadic function.
+	fn(slice...)
 }
