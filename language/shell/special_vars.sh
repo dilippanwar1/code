822f92e75e413c9124e0d8d209cbdc80fe128c7d
Deyuan Deng <deyuan.deng@gmail.com>
2014-11-15 20:46:55 -0500

    Bash example.

--- language/shell/special_vars.sh
+++ language/shell/special_vars.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-
 # $0 is the name of the shell script; sometimes used to get directory.
 echo $0
 # This is somewhat equivalent to $0.
 echo ${BASH_SOURCE[0]}
+echo $(dirname "${BASH_SOURCE}")
 
+echo $1                         # First param
+echo $2                         # Second param
+echo "${1}"                     # Also First param
 
 # $! is PID of the most recent background command.
 sleep 1 &
@@ -14,16 +17,14 @@ echo $!
 # pid of the current shell (not subshell)
 echo $$
 
-
 # A string representation of a params; run 'bash special_vars.sh hi ddy.'
 # will print: hi ddy.
 echo $@
-
+echo "$@"                       # The same as $@
 echo ${@:2}
 
 # $# returns the number of parameters
 echo $#
-
 
 # $? is used to find the error code of the last executed command.
 ls xxx
--- language/shell/variables.sh
+++ language/shell/variables.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
-
-# If CONFIG_FILE is empty, then print 'config-default.sh'; if CONFIG_FILE
-# is not empty, then print CONFIG_FILE.
+# If CONFIG_FILE is empty, then print 'config-default.sh';
+# if CONFIG_FILE is not empty, then print CONFIG_FILE.
 echo ${CONFIG_FILE-"config-default.sh"}
 
 # Same as above.
 API_PORT=${API_PORT:-8080}
 echo $API_PORT
 
-
-# Shell variable that returns a random number.
+# RANDOM is shell variable that returns a random number.
 echo $RANDOM
 
+# PWD is shell variable that returns a current working directory.
+echo $PWD
+echo $(pwd)
 
-# EXIT is built-in. The echo command is executed when shell exits, either
-# normally or upon receiving signals.
+# EXIT is built-in. The echo command is executed when shell exits,
+# either normally or upon receiving signals.
 trap "echo Bye from exit" EXIT
 
-sleep 3
+sleep 1
