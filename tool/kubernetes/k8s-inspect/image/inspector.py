a8d564a1af39d396a5d1244488c6c668adca6d47
Deyuan Deng <deyuan.deng@gmail.com>
2014-11-22 23:42:37 -0500

    k8s script

--- tool/kubernetes/k8s-inspect/image/inspector.py
+++ tool/kubernetes/k8s-inspect/image/inspector.py
@@ -1,6 +1,19 @@
 import netifaces
+import os
 
-for interface in netifaces.interfaces():
-  if netifaces.AF_INET in netifaces.ifaddresses(interface):
-    print interface
-    print netifaces.ifaddresses(interface)[netifaces.AF_INET]
+
+def PrintInterfaces():
+  """Print all interfaces in the Pod."""
+  for interface in netifaces.interfaces():
+    if netifaces.AF_INET in netifaces.ifaddresses(interface):
+      print interface
+      print netifaces.ifaddresses(interface)[netifaces.AF_INET]
+
+
+def PrintEnv():
+  """Print all environment variables in the Pod."""
+  print os.environ
+
+
+if __name__ == '__main__':
+  PrintEnv()
