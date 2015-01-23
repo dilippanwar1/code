db0a4e8d3cfbab22f8ac084adbe1f2cbc9fd1709
Deyuan Deng <deyuan.deng@gmail.com>
2015-01-22 23:13:39 -0500

    Go reflect and time

--- language/go/example/pkg_reflect_deepequal.go
+++ language/go/example/pkg_reflect_deepequal.go
@@ -8,6 +8,17 @@ import (
 func main() {
 	slice1 := []string{"1", "2"}
 	slice2 := []string{"2", "1"}
-
 	fmt.Println(reflect.DeepEqual(slice1, slice2)) // False
+
+	slice3 := []string{}
+	slice4 := []string{}
+	fmt.Println(reflect.DeepEqual(slice3, slice4)) // True
+
+	slice5 := []string{}
+	var slice6 []string
+	fmt.Println(reflect.DeepEqual(slice5, slice6)) // False
+
+	map1 := map[string]string{"ddy": "Deyuan Deng", "lsj": "Sijia Luo"}
+	map2 := map[string]string{"lsj": "Sijia Luo", "ddy": "Deyuan Deng"}
+	fmt.Println(reflect.DeepEqual(map1, map2)) // True
 }
--- language/go/example/pkg_time.go
+++ language/go/example/pkg_time.go
@@ -1,12 +1,20 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"time"
 )
 
 func main() {
+
+	periodPtr := flag.Duration("period", 10*time.Second, "The period flag.")
+
+	flag.Parse()
+
 	t := time.Second
 	fmt.Println(t)
 	fmt.Println(int(t))
+
+	fmt.Println(*periodPtr)
 }
