2e081eba5b035e45ee24863570e4a11897e337c8
Deyuan Deng <deyuan.deng@gmail.com>
2015-01-27 14:40:29 -0500

    etcd experiment

--- language/go/example/pkg_sort.go
+++ language/go/example/pkg_sort.go
@@ -1,35 +1,76 @@
 package main
 
-type TypeMeta struct {
-	Kind       string
-	APIVersion string
+import (
+	"fmt"
+	"sort"
+	"time"
+)
+
+type Node struct {
+	Key           string     `json:"key, omitempty"`
+	Value         string     `json:"value,omitempty"`
+	Dir           bool       `json:"dir,omitempty"`
+	Expiration    *time.Time `json:"expiration,omitempty"`
+	TTL           int64      `json:"ttl,omitempty"`
+	Nodes         Nodes      `json:"nodes,omitempty"`
+	ModifiedIndex uint64     `json:"modifiedIndex,omitempty"`
+	CreatedIndex  uint64     `json:"createdIndex,omitempty"`
 }
 
-type ListMeta struct {
-	SelfLink        string
-	ResourceVersion string
+type Nodes []*Node
+
+// interfaces for sorting
+func (ns Nodes) Len() int {
+	return len(ns)
 }
 
-type ObjectMeta struct {
-	Name      string
-	Namespace string
+func (ns Nodes) Less(i, j int) bool {
+	fmt.Println(ns[i].Key)
+	return ns[i].Key < ns[j].Key
 }
 
-type Controller struct {
-	TypeMeta
-	ObjectMeta
-
-	Replica  int
-	Selector map[string]string
-}
-
-type ControllerList struct {
-	TypeMeta
-	ListMeta
-
-	Items []Controller
+func (ns Nodes) Swap(i, j int) {
+	ns[i], ns[j] = ns[j], ns[i]
 }
 
 func main() {
+	node := Node{
+		Nodes: Nodes{
+			{
+				Value: `{"name": "directory1"}`,
+				Dir:   true,
+				Nodes: Nodes{
+					{
+						Value:         `{"id":"foo","kind":"Pod","apiVersion":"v1beta1"}`,
+						ModifiedIndex: 1,
+					},
+				},
+			},
+			{
+				Value: `{"name": "directory3"}`,
+				Dir:   true,
+				Nodes: Nodes{
+					{
+						Value:         `{"id":"bar","kind":"Pod","apiVersion":"v1beta1"}`,
+						ModifiedIndex: 2,
+					},
+				},
+			},
+			{
+				Value: `{"name": "directory2"}`,
+				Dir:   true,
+				Nodes: Nodes{
+					{
+						Value:         `{"id":"bar","kind":"Pod","apiVersion":"v1beta1"}`,
+						ModifiedIndex: 2,
+					},
+				},
+			},
+		},
+	}
 
+	sort.Sort(node.Nodes)
+	fmt.Println(node.Nodes[0])
+	fmt.Println(node.Nodes[1])
+	fmt.Println(node.Nodes[2])
 }
--- tool/etcd/key_order.go
+++ tool/etcd/key_order.go
@@ -27,20 +27,26 @@ func main() {
 	// Create three keys.
 	client.Set("/key_order/msg1", "{Name:Deyuan,Age:25}", 0)
 	client.Set("/key_order/msg3", "{Name:Deyuan,Age:27}", 0)
+	client.Set("/key_order/msg4/gsm2", "{Name:Deyuan,Age:22}", 0)
 	client.Set("/key_order/msg2", "{Name:Deyuan,Age:26}", 0)
+	client.Set("/key_order/msg4/gsm1", "{Name:Deyuan,Age:21}", 0)
 	// Modify one key.
 	client.Set("/key_order/msg2", "{Name:Deyuan,Age:11}", 0)
 
 	// If no sort param is given, the order is last created (observation), by we
 	// can not trust it.
-	resp, _ := client.Get("/key_order", false, false)
+	resp, _ := client.Get("/key_order", false, true)
+	fmt.Println("------------------- unsorted --------------------")
 	fmt.Println(resp.Node.Nodes[0])
 	fmt.Println(resp.Node.Nodes[1])
 	fmt.Println(resp.Node.Nodes[2])
+	fmt.Println(resp.Node.Nodes[3])
 
 	// If sort param is given, the result is sorted by key.
-	resp, _ = client.Get("/key_order", true, false)
+	fmt.Println("------------------- sorted --------------------")
+	resp, _ = client.Get("/key_order", true, true)
 	fmt.Println(resp.Node.Nodes[0])
 	fmt.Println(resp.Node.Nodes[1])
 	fmt.Println(resp.Node.Nodes[2])
+	fmt.Println(resp.Node.Nodes[3])
 }
