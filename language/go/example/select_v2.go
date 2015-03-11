8f4d5b936a1124c059228848be078d16c9d44986
Deyuan Deng <deyuan.deng@gmail.com>
2015-03-11 19:35:06 -0400

    Go select

--- language/go/example/select_v2.go
+++ language/go/example/select_v2.go
@@ -1,56 +1,37 @@
+// Go’s select lets you wait on multiple channel operations. Combining
+// goroutines and channels with select is a powerful feature of Go. A
+// select blocks until one of its cases can run, then it executes that
+// case. It chooses one at random if multiple are ready.
 package main
 
 import (
 	"fmt"
-	"strconv"
 	"time"
 )
 
-func makeCakeAndSend(cs chan string, flavor string, count int) {
-	for i := 1; i <= count; i++ {
-		cakeName := flavor + " Cake " + strconv.Itoa(i)
-		cs <- cakeName
-	}
-	close(cs)
-}
-
-func receiveCakeAndPack(strbry_cs chan string, choco_cs chan string) {
-	strbry_closed, choco_closed := false, false
-
-	for {
-		// If both channels are closed then we can stop.
-		if strbry_closed && choco_closed {
-			fmt.Println("Both channels closed.")
-			return
-		}
-		select {
-		case cakeName, strbry_ok := <-strbry_cs:
-			if !strbry_ok {
-				strbry_closed = true
-			} else {
-				fmt.Println("Received from Strawberry channel.  Now packing", cakeName)
-			}
-		case cakeName, choco_ok := <-choco_cs:
-			if !choco_ok {
-				choco_closed = true
-			} else {
-				fmt.Println("Received from Chocolate channel.  Now packing", cakeName)
-			}
-		}
-	}
-}
-
 func main() {
-	strbry_cs := make(chan string)
-	choco_cs := make(chan string)
 
-	// Two cake makers
-	go makeCakeAndSend(choco_cs, "Chocolate", 3)
-	go makeCakeAndSend(strbry_cs, "Strawberry", 3)
+	c1 := make(chan string)
+	c2 := make(chan string)
 
-	// Pack all cakes received on these cake channels.
-	go receiveCakeAndPack(strbry_cs, choco_cs)
+	// Each channel will receive a value after some amount of time.
+	go func() {
+		time.Sleep(time.Second * 1)
+		c1 <- "one"
+	}()
 
-	// Sleep for a while so that the program doesn’t exit immediately
-	time.Sleep(2 * time.Second)
+	go func() {
+		time.Sleep(time.Second * 2)
+		c1 <- "two"
+	}()
+
+	// Use select to wait both of these values simultaneously.
+	for i := 0; i < 2; i++ {
+		select {
+		case msg1 := <-c1:
+			fmt.Println("received", msg1)
+		case msg2 := <-c2:
+			fmt.Println("received", msg2)
+		}
+	}
 }
