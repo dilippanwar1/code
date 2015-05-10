3cae1acb09fad11c6bb230631f110b38a5c065ad
Deyuan Deng <deyuan.deng@gmail.com>
2015-05-10 00:09:24 -0400

    Docker go client

--- language/go/example/bool.go
+++ language/go/example/bool.go
@@ -6,6 +6,12 @@ func main() {
 	fmt.Println(isValid("abc", 20)) // true
 	fmt.Println(isValid("abc", 16)) // false
 	fmt.Println(isValid("xxx", 20)) // false
+
+	// Pointer to bool.
+	var boolp *bool
+	true_var := true
+	boolp = &true_var
+	fmt.Println(*boolp)
 }
 
 func isValid(name string, age int) bool {
--- tool/docker/go-client/main.go
+++ tool/docker/go-client/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	docker "github.com/fsouza/go-dockerclient"
 )
@@ -9,10 +10,12 @@ import (
 func main() {
 	endpoint := "unix:///var/run/docker.sock"
 	client, _ := docker.NewClient(endpoint)
+	var err error
+	var opts docker.PullImageOptions
 
-	images, _ := client.ListImages(true)
-	for _, image := range images {
-		if false {
+	if false {
+		images, _ := client.ListImages(docker.ListImagesOptions{All: true})
+		for _, image := range images {
 			fmt.Println("ID:", image.ID)
 			fmt.Println("RepoTags:", image.RepoTags)
 			fmt.Println("Created: ", image.Created)
@@ -22,8 +25,67 @@ func main() {
 	}
 
 	// "All" option will list all (running + stopped) containers.
-	containers, _ := client.ListContainers(docker.ListContainersOptions{All: true})
-	for _, container := range containers {
-		fmt.Println("ID:", container.ID)
+	if false {
+		containers, _ := client.ListContainers(docker.ListContainersOptions{All: true})
+		for _, container := range containers {
+			if false {
+				fmt.Println("ID:", container.ID)
+			}
+		}
+	}
+
+	if false {
+		opts := docker.PullImageOptions{Repository: "python:2.7", OutputStream: os.Stdout}
+		err := client.PullImage(opts, docker.AuthConfiguration{})
+		if err != nil {
+			fmt.Println("Error pulling images:", err)
+		} else {
+			fmt.Println("Image pulled")
+		}
+	}
+
+	// Equivalent with the above option.
+	if false {
+		opts = docker.PullImageOptions{Repository: "python", Tag: "2.7", OutputStream: os.Stdout}
+		err = client.PullImage(opts, docker.AuthConfiguration{})
+		if err != nil {
+			fmt.Println("Error pulling images:", err)
+		} else {
+			fmt.Println("Image pulled")
+		}
+	}
+
+	if false {
+		opts = docker.PullImageOptions{Repository: "python", Tag: "2.7", OutputStream: os.Stdout}
+		err = client.PullImage(opts, docker.AuthConfiguration{})
+		if err != nil {
+			fmt.Println("Error pulling images:", err)
+		} else {
+			fmt.Println("Image pulled")
+		}
+	}
+
+	// Pull from another registry.
+	if true {
+		opts = docker.PullImageOptions{Repository: "gcr.io/google_containers/pause", OutputStream: os.Stdout}
+		err = client.PullImage(opts, docker.AuthConfiguration{})
+		if err != nil {
+			fmt.Println("Error pulling images:", err)
+		} else {
+			fmt.Println("Image pulled")
+		}
+	}
+
+	// 'ddysher/musicjsns' is a private repo, we must provide credential.  Failing to provide
+	// credential (wrong credential or no credential) will results in 'image not found' error.
+	if false {
+		opts = docker.PullImageOptions{Repository: "ddysher/musicjsns", OutputStream: os.Stdout}
+		auths, _ := docker.NewAuthConfigurationsFromDockerCfg()
+		err = client.PullImage(opts, auths.Configs["https://index.docker.io/v1/"])
+		if err != nil {
+			fmt.Println("Error pulling images:", err)
+		} else {
+			fmt.Println("Image pulled")
+		}
 	}
 }
