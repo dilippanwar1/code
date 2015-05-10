5152faacd3d055f39f274f60a9cf8db2b627f8fe
Deyuan Deng <deyuan.deng@gmail.com>
2015-05-10 11:31:24 -0400

    Google managed vm

---
+++ tool/appengine/managed-vms/main.py
@@ -0,0 +1,28 @@
+# $ gcloud preview app run ./app.yaml
+# $ gcloud preview app deploy .yaml
+
+from flask import Flask, helpers
+import subprocess
+import tempfile
+
+from synth import Synth
+
+app= Flask(__name__)
+app.debug = True
+
+synth = Synth()
+
+MESSAGES = '/tmp/messages.txt'
+
+@app.route("/")
+def fortune():
+  msg = subprocess.check_output('/usr/games/fortune')
+  with open(MESSAGES, 'a') as f:
+    f.write(msg)
+  with tempfile.NamedTemporaryFile() as f:
+    synth.say(msg, out=f)
+    return helpers.send_file(f.name, mimetype="audio/wav", as_attachment=False)
+
+@app.route("/messages")
+def messages():
+  return helpers.send_file(MESSAGES)
