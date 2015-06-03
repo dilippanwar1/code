dad6ac9e144664cdc5be98613e64ba41964f6dd4
Deyuan Deng <deyuan@google.com>
2015-06-03 16:42:41 -0400

    Misc changes

--- language/python/string.py
+++ language/python/string.py
@@ -10,3 +10,10 @@ print 'pcr12345pcr'.rstrip('pcr')
 # string.strip, return a copy of the string with leading and trailing
 # characters removed
 print 'pcr12345pcr'.strip('pcr')
+
+print 'localhost:8000:1020'.rfind(':')
+print 'localhost:8000:1020'.find(':')
+
+power_device = '32:CD1-B2-2B-6-1:meter'
+colon = power_device.rfind(':')
+print power_device[:colon], power_device[colon:]
