ac0d9ff1af48b40a8d5cfb7d8a70e882e0b9d724
Deyuan Deng <deyuan.deng@gmail.com>
2015-06-05 09:42:01 -0400

    etcd tool

--- tool/etcd/basic.go
+++ tool/etcd/basic.go
@@ -11,7 +11,8 @@ import (
 )
 
 // A thin wrapper around client.Watch to print info.
-func watchWrapper(client *etcd.Client, prefix string, waitIndex uint64, recursive bool, receiver chan *etcd.Response, stop chan bool) {
+func watchWrapper(client *etcd.Client, prefix string, waitIndex uint64,
+	recursive bool, receiver chan *etcd.Response, stop chan bool) {
 	fmt.Println("============================================================")
 	fmt.Println("Start longterm watching")
 	client.Watch(prefix, waitIndex, recursive, receiver, stop)
@@ -22,7 +23,7 @@ func main() {
 	client := etcd.NewClient([]string{})
 
 	// Set key "/message" with value. Since etcd use file-system like structure,
-	// keys are always start with '/'.
+	// keys always start with '/'.
 	fmt.Println("============================================================")
 	fmt.Println("Set initial message")
 	resp, err := client.Set("/message", "{Name:Deyuan,Age:25}", 0)
@@ -47,12 +48,12 @@ func main() {
 	client.Watch("/message", 5, false, nil, nil)
 	fmt.Println("Stop  watching on /message index 5")
 
-	// When passing 'etcdIncoming' to client.Watch, it is a long term watch.
-	// client.Watch will block at the channel. After someone receives the channel,
-	// it will go on to watch that prefix. If a stop channel is given, the client
-	// can close long-term watch using the stop channel.
+	// When passing 'etcdIncoming' to client.Watch, it is a long term watch. If
+	// new change becomes available, client.Watch will block at the channel until
+	// someone read from it - it will go on and watch the key again. If a stop
+	// channel is given, the client can close long-term watch using the channel.
 	// - If wait index is 0, it watches on latest changes, which means Watch will
-	//   block immediately, until new changes comes.
+	//   block immediately, until new change comes.
 	// - If wait index is not 0, and is smaller than current key's index, then all
 	//   versions from wait index to current index will be listed. E.g. If message
 	//   key is already 8, and since we are watching on 5, then index 5,6,7 will
@@ -61,7 +62,7 @@ func main() {
 	// - If wait index is not 0, and is larger than current key's index, then
 	//   Watch will wait until index reaches wait index. E.g. If message key is
 	//   2 and we are watching on 5, then nothing will happen between index 2,3,4.
-	//   When index reaches 5, Watch starts send changes to etcdIncoming.
+	//   When index reaches 5, Watch starts sending changes to etcdIncoming.
 	etcdIncoming := make(chan *etcd.Response)
 	etcdStop := make(chan bool)
 	go watchWrapper(client, "/message", 5, false, etcdIncoming, etcdStop)
