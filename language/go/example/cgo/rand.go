f21aa72b79ebdbf076339cd03a5390012c006b5b
Deyuan Deng <deyuan.deng@gmail.com>
2015-12-31 15:42:04 +0800

    Go explore

---
+++ language/go/example/cgo/rand.go
@@ -0,0 +1,21 @@
+// http://blog.golang.org/c-go-cgo
+
+package main
+
+/*
+#include <stdlib.h>
+*/
+import "C"
+import "fmt"
+
+func Random() int {
+	return int(C.random())
+}
+
+func Seed(i int) {
+	C.srandom(C.uint(i))
+}
+
+func main() {
+	fmt.Println(Random())
+}
