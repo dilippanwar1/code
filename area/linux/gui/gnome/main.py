8879c89ee32568bda885756bccace6d460b75611
Deyuan Deng <deyuan.deng@gmail.com>
2017-02-05 11:26:54 +0800

    gtk, gnome, x11

---
+++ area/linux/gui/gnome/main.py
@@ -0,0 +1,23 @@
+# Taken from: https://developer.gnome.org/gnome-devel-demos/stable/window.py.html.en
+#
+# $ python main.py
+
+from gi.repository import Gtk
+import sys
+
+
+class MyApplication(Gtk.Application):
+
+    def do_activate(self):
+        # create a Gtk Window belonging to the application itself
+        window = Gtk.Window(application=self)
+        # set the title
+        window.set_title("Welcome to GNOME")
+        # show the window
+        window.show_all()
+
+# create and run the application, exit with the value returned by
+# running the program
+app = MyApplication()
+exit_status = app.run(sys.argv)
+sys.exit(exit_status)
