cac66f56ae82f475f66fe691751200eee4cbf8a6
Deyuan Deng <deyuan@caicloud.io>
2017-06-13 16:28:45 +0800

    Go template and path

--- language/go/example/pkg_path/main.go
+++ language/go/example/pkg_path/main.go
@@ -20,4 +20,17 @@ func main() {
 
 	name = "/var/lib/kubelet/pods/b9a14925-320d-11e7-b142-8825937fa049/volumes/kubernetes.io~local-volume/local-pv"
 	fmt.Println(filepath.Split(name))
+
+	paths := []string{
+		"/a/b/c",
+		"/b/c",
+		"./b/c",
+	}
+	base := "/a"
+
+	fmt.Println("Join(basepath, Rel(basepath, targpath)) is equivalent to targpath itself.")
+	for _, p := range paths {
+		rel, err := filepath.Rel(base, p)
+		fmt.Printf("base: %q target: %q\t --> result: %v\terror: err%v\n", base, p, rel, err)
+	}
 }
--- language/go/example/pkg_template/template.go
+++ language/go/example/pkg_template/template.go
@@ -5,11 +5,15 @@ import (
 	"os"
 )
 
+func main() {
+	fileTemplate()
+}
+
 type Person struct {
 	Name string
 }
 
-func main() {
+func simpleTemplate() {
 	// Create a template with a name.
 	t := template.New("Example")
 
@@ -19,4 +23,17 @@ func main() {
 
 	p := Person{Name: "Mary"}
 	t.Execute(os.Stdout, p)
+}
+
+type ConfigToYaml struct {
+	Volumes      map[string]string
+	VolumeMounts map[string]string
+}
+
+func fileTemplate() {
+	t, _ := template.ParseFiles("k8s_pod.yaml.tmpl") //setp 1
+	t.Execute(os.Stdout, ConfigToYaml{
+		Volumes:      map[string]string{"vvv1": "/mnt/fast", "vvv2": "/mnt/slow"},
+		VolumeMounts: map[string]string{"vvv1": "/local-fast", "vvv2": "/local-slow"},
+	})
 }
